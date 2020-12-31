84a3ba0e7dd0dfcfa03e3b341d595c0f6a121655
Ivan Fernandez <ivanfernandez422000@gmail.com>
2020-12-31 16:57:28 +0100

    To continue. Section 12 -- Lesson 144

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -49,6 +49,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ],
   providers: [
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { RegisterUser } from '../_models/register-user';
 import { AccountService } from '../_services/account.service';
@@ -11,38 +12,54 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Input() usersFromHomeComponent: any;
   @Output() closeRegisterEvent = new EventEmitter();
-  userToRegisterForm: any = {};
+  //userToRegisterForm: any = {};
+
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
-  registerUser(){
-    if(this.isValidPassword()){
-      const userToRegister: RegisterUser = 
-      {
-        userName:this.userToRegisterForm.username, 
-        password:this.userToRegisterForm.password
-      };
-
-      this.accountService.registerUser(userToRegister).subscribe((res) => {
-        this.toastr.success("Register successful");
-        this.cancel();
-      }, error => {
-        this.toastr.error(error.error);
-        console.log(error);
-      });
-    }else{
-      this.toastr.error("Passwords don't match");
-    }
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.arePasswordsTheSame])
+    });
   }
 
-  cancel(){
+  arePasswordsTheSame: ValidatorFn = (control: AbstractControl) => {
+    if(control.value == control.parent?.controls['password']?.value)
+      return null;
+
+    return { matchError: true};
+  }
+
+  registerUser() {
+    // const userToRegister: RegisterUser = 
+    // {
+    //   userName:this.userToRegisterForm.username, 
+    //   password:this.userToRegisterForm.password
+    // };
+
+    const userToRegister: RegisterUser = 
+    {
+      userName:this.registerForm.get("username").value, 
+      password:this.registerForm.get("password").value
+    };
+
+    this.accountService.registerUser(userToRegister).subscribe((res) => {
+      this.toastr.success("Register successful");
+      this.cancel();
+    }, error => {
+      this.toastr.error(error.error);
+      console.log(error);
+    });
+  }
+
+  cancel() {
     this.closeRegisterEvent.emit(false);
-  }
-
-  private isValidPassword(){
-    return this.userToRegisterForm.password.trim() == this.userToRegisterForm.repeatPassword.trim();
   }
 }
