1375a242b14b17540d559f2256c1063b2b60ff42
Ivan Fernandez <ivanfernandez422000@gmail.com>
2021-02-20 12:42:16 +0100

    Section 19 -- Fix FallbackController extends class

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class FallbackController : BaseApiController
+    public class FallbackController : Controller
     {
         public ActionResult Index()
         {
