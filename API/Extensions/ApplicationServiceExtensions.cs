673516234179ce790393611a8448e08fbaeac643
Ivan Fernandez <ivanfernandez422000@gmail.com>
2021-02-20 13:40:30 +0100

    Section 19 -- Delete build

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -28,20 +28,20 @@ namespace API.Extensions
 
                 string connStr;
 
-                    // Depending on if in development or production, use either Heroku-provided
-                    // connection string, or development connection string from env var.
-                    if (env == "Development")
+                // Depending on if in development or production, use either Heroku-provided
+                // connection string, or development connection string from env var.
+                if (env == "Development")
                 {
-                        // Use connection string from file.
-                        connStr = config.GetConnectionString("DefaultConnection");
+                    // Use connection string from file.
+                    connStr = config.GetConnectionString("DefaultConnection");
                 }
                 else
                 {
-                        // Use connection string provided at runtime by Heroku.
-                        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-                        // Parse connection URL to connection string for Npgsql
-                        connUrl = connUrl.Replace("postgres://", string.Empty);
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
                     var pgHostPortDb = connUrl.Split("@")[1];
                     var pgHostPort = pgHostPortDb.Split("/")[0];
@@ -54,9 +54,9 @@ namespace API.Extensions
                     connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
                 }
 
-                    // Whether the connection string came from the local development configuration file
-                    // or from the environment variable from Heroku, use it to set up your DbContext.
-                    options.UseNpgsql(connStr);
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from Heroku, use it to set up your DbContext.
+                options.UseNpgsql(connStr);
             });
         }
     }
